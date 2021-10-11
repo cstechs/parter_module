891ffb5db2944f76933c003ceb77ed7b6a4e8af3
cstechs <cstechfreelancing@gmail.com>
2021-10-11 17:37:27 +0500

    student table completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import DashboardLayout from './components/Dashboard/DashboardLayout/DashboardLayout';
-import MockTest from './components/Dashboard/MockTest/MockTest';
-import {Route, Switch} from 'react-router'
+import "./App.css";
+import DashboardLayout from "./components/Dashboard/DashboardLayout/DashboardLayout";
+import MockTest from "./components/Dashboard/MockTest/MockTest";
+import Trainers from "./components/Dashboard/UserAccount/Trainers/index";
+import Students from "./components/Dashboard/UserAccount/Students";
+import { Route, Switch } from "react-router";
 
 function App() {
   return (
     <DashboardLayout>
       <Route path="/mock-test" component={MockTest} />
+      <Route path="/students" component={Students} />
+      {/* <Route path="/trainers" component={Trainers} /> */}
     </DashboardLayout>
   );
 }
