38a6ab5aaad336e2434e60ed845d41b089c70dc0
malaikacstech <malaika@cstechsoftwares.com>
2021-10-12 15:54:29 +0500

    changes

--- src/components/Dashboard/MockTest/ScoredView/ScoredView.jsx
+++ src/components/Dashboard/MockTest/ScoredView/ScoredView.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
+import { Menu, Grid , Button } from "@mui/material";
 import './styles.scss'
 
 const ScoredView = () =>{
     return (
       <div className="scored-container">
-        <div className="scored-div">
-          <div className="ques-no">71</div>
-          <div className="ques-heading">Questions</div>
-          <div className="mins">2 Hr 46 Min</div>
-          <div className="test-tag">Scored Mocked Test A</div>
-        </div>
-        <div className="scored-div">
-          <div className="ques-no">71</div>
-          <div className="ques-heading">Questions</div>
-          <div className="mins">2 Hr 46 Min</div>
-          <div className="test-tag">Scored Mocked Test A</div>
-        </div>
-        <div className="scored-div">
-          <div className="ques-no">71</div>
-          <div className="ques-heading">Questions</div>
-          <div className="mins">2 Hr 46 Min</div>
-          <div className="test-tag">Scored Mocked Test A</div>
-        </div>
-        <div className="scored-div">
-          <div className="ques-no">71</div>
-          <div className="ques-heading">Questions</div>
-          <div className="mins">2 Hr 46 Min</div>
-          <div className="test-tag">Scored Mocked Test A</div>
-        </div>
+        <Grid container spacing>
+          <Grid item md={4} xs={6}> 
+            <div className="scored-div">
+              <div className="ques-no">71</div>
+              <div className="ques-heading">Questions</div>
+              <div className="mins">2 Hr 46 Min</div>
+              <div className="test-tag">Scored Mocked Test A</div>
+            </div>
+          </Grid>
+          <Grid item md={4} xs={6}>
+            <div className="scored-div">
+              <div className="ques-no">71</div>
+              <div className="ques-heading">Questions</div>
+              <div className="mins">2 Hr 46 Min</div>
+              <div className="test-tag">Scored Mocked Test A</div>
+            </div>
+          </Grid>
+          <Grid item md={4} xs={6}>
+            <div className="scored-div">
+              <div className="ques-no">71</div>
+              <div className="ques-heading">Questions</div>
+              <div className="mins">2 Hr 46 Min</div>
+              <div className="test-tag">Scored Mocked Test A</div>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     );
 }
