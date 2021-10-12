e39abc865b5a96862e72d04a21e5c3bcdec6d437
malaikacstech <malaika@cstechsoftwares.com>
2021-10-12 11:20:29 +0500

    Added section wise view

--- src/components/Dashboard/DashboardLayout/DashboardHeader/DashboardHeader.jsx
+++ src/components/Dashboard/DashboardLayout/DashboardHeader/DashboardHeader.jsx
@@ -1,41 +1,56 @@
-import React from 'react'
-import {Menu, Grid, Button} from '@mui/material'
-import {MenuOpen} from '@mui/icons-material'
-import dashboardLogo from '../../../../assests/dashboardLogo.svg'
-import profile from '../../../../assests/profile.png'
-import notificationIcon from '../../../../assests/notificationIcon.svg'
-import './styles.scss'
+import React from "react";
+import { Menu, Grid, Button } from "@mui/material";
+import { MenuOpen, ContactMail } from "@mui/icons-material";
+import dashboardLogo from "../../../../assests/dashboardLogo.svg";
+import profile from "../../../../assests/profile.png";
+import notificationIcon from "../../../../assests/notificationIcon.svg";
+import "./styles.scss";
 
-const DashboardHeader = ({drawerState, setDrawerState}) =>{
-    return(
-        <div className="header-div">
-        <Grid container position="fixed" centered spacing={3} className="grid-container">
-            <Grid item md={2} sm={3} xs={3} className="grid-item">
-            <div className="logo-div">
-                <img src={dashboardLogo} alt="logo" />
-                <div className="logo-title">Logo</div>
-            </div>
-            </Grid>
-            <Grid item md={7} sm={5} xs={0} className='grid-item'>
-                <div className="welcome-header">
-                    Welcome user name! We are super excited to have you on board. Best Wishes.
-                </div>
-            </Grid>
-            <Grid item md={3} sm={4} xs={8} className="grid-item">
-                <div className="profile-div">
-                    <Button variant="outlined">
-                        Contact us
-                    </Button>
-                    <img src={notificationIcon} alt="notifications" className="notification"/>
-                    <img src={profile} alt="profile avatar" className="avatar-img"/>
-                    <div className='hamburger-icon' onClick={()=> setDrawerState(true)}>
-                        <MenuOpen />
-                    </div>
-                </div>
-            </Grid>
+const DashboardHeader = ({ drawerState, setDrawerState }) => {
+  return (
+    <div className="header-div">
+      <Grid
+        container
+        position="fixed"
+        centered
+        spacing={3}
+        className="grid-container"
+      >
+        <Grid item md={2} sm={3} xs={3} className="grid-item">
+          <div className="logo-div">
+            <img src={dashboardLogo} alt="logo" />
+            <div className="logo-title">Logo</div>
+          </div>
         </Grid>
-        </div>
-    )
-}
+        <Grid item md={7} sm={5} xs={0} className="grid-item">
+          <div className="welcome-header">
+            Welcome user name! We are super excited to have you on board. Best
+            Wishes.
+          </div>
+        </Grid>
+        <Grid item md={3} sm={4} xs={8} className="grid-item">
+          <div className="profile-div">
+            <Button variant="outlined" className="contact-button">
+              Contact us
+            </Button>
+            <ContactMail className="contact-mail" />
+            <img
+              src={notificationIcon}
+              alt="notifications"
+              className="notification"
+            />
+            <img src={profile} alt="profile avatar" className="avatar-img" />
+            <div
+              className="hamburger-icon"
+              onClick={() => setDrawerState(true)}
+            >
+              <MenuOpen />
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default DashboardHeader
+export default DashboardHeader;
--- src/components/Dashboard/MockTest/SectionView/SectionView.jsx
+++ src/components/Dashboard/MockTest/SectionView/SectionView.jsx
@@ -1,21 +1,104 @@
-import React from 'react'
-import {Grid} from '@mui/material'
-import { ReactComponent as MicIcon } from "../../../../assests/sectionView/micIcon.svg";
-import { ReactComponent as ForwardIcon } from "../../../../assests/sectionView/forwardIcon.svg";
-import {MicOutlined} from '@mui/icons-material'
+import React from 'react';
+import {Grid} from '@mui/material';
+import micIcon from '../../../../assests/sectionView/micIcon.svg';
+import forwardIcon from '../../../../assests/sectionView/forwardIcon.svg';
+import listeningIcon from '../../../../assests/sectionView/listeningIcon.svg';
+import readingIcon from '../../../../assests/sectionView/readingIcon.svg'
+import writingIcon from '../../../../assests/sectionView/writingIcon.svg'
 import './styles.scss'
 
 const SectionView = () =>{
     return (
       <div className="section-container">
         <div className="section-row">
-            <div className="right-div">
-                <MicIcon />
-                <div>Speaking</div>
+          <div className="left-div">
+            <img src={micIcon} alt="Speaking" />
+            <div className="section-title">Speaking</div>
+            <img src={forwardIcon} alt="Speaking" />
+          </div>
+          <div className="right-div">
+            <div className="test-number">Speaking Test A</div>
+            <div className="duration-div">
+              Duration
+              <div className="duration">35 mins</div>
             </div>
-            <div className="right-div">
-
-            </div>  
+            <div className="questions-div">
+              No. Of Questions
+              <div className="questions">30</div>
+            </div>
+          </div>
+        </div>
+        <div className="section-row">
+          <div className="left-div">
+            <img src={micIcon} alt="Speaking" />
+            <div className="section-title">Speaking</div>
+            <img src={forwardIcon} alt="Speaking" />
+          </div>
+          <div className="right-div">
+            <div className="test-number">Speaking Test A</div>
+            <div className="duration-div">
+              Duration
+              <div className="duration">35 mins</div>
+            </div>
+            <div className="questions-div">
+              No. Of Questions
+              <div className="questions">30</div>
+            </div>
+          </div>
+        </div>
+        <div className="section-row">
+          <div className="left-div">
+            <img src={micIcon} alt="Speaking" />
+            <div className="section-title">Speaking</div>
+            <img src={forwardIcon} alt="Speaking" />
+          </div>
+          <div className="right-div">
+            <div className="test-number">Speaking Test A</div>
+            <div className="duration-div">
+              Duration
+              <div className="duration">35 mins</div>
+            </div>
+            <div className="questions-div">
+              No. Of Questions
+              <div className="questions">30</div>
+            </div>
+          </div>
+        </div>
+        <div className="section-row">
+          <div className="left-div">
+            <img src={micIcon} alt="Speaking" />
+            <div className="section-title">Speaking</div>
+            <img src={forwardIcon} alt="Speaking" />
+          </div>
+          <div className="right-div">
+            <div className="test-number">Speaking Test A</div>
+            <div className="duration-div">
+              Duration
+              <div className="duration">35 mins</div>
+            </div>
+            <div className="questions-div">
+              No. Of Questions
+              <div className="questions">30</div>
+            </div>
+          </div>
+        </div>
+        <div className="section-row">
+          <div className="left-div">
+            <img src={micIcon} alt="Speaking" />
+            <div className="section-title">Speaking</div>
+            <img src={forwardIcon} alt="Speaking" />
+          </div>
+          <div className="right-div">
+            <div className="test-number">Speaking Test A</div>
+            <div className="duration-div">
+              Duration
+              <div className="duration">35 mins</div>
+            </div>
+            <div className="questions-div">
+              No. Of Questions
+              <div className="questions">30</div>
+            </div>
+          </div>
         </div>
       </div>
     );
